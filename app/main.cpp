33760167abe0d3ab21f321d3a5f47707206486fa
ronald112 <ronald11211@gmail.com>
2020-11-13 21:23:24 +0200

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setApplicationName("Player Example");
+    QCoreApplication::setOrganizationName("UNIT Study project");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
+
+
     UAmp w;
     w.show();
     return a.exec();
