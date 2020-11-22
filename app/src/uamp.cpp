5d09ba7192d7e715577bc6b49446d4c648f2f378
ailchuk <an.ilchuk27@gmail.com>
2020-11-22 19:08:44 +0200

    add new songs to tableview

--- app/src/uamp.cpp
+++ app/src/uamp.cpp
@@ -1,4 +1,4 @@
-    #include "uamp.h"
+#include "uamp.h"
 #include "./ui_uamp.h"
 
 UAmp::UAmp(QWidget *parent)
@@ -6,6 +6,24 @@ UAmp::UAmp(QWidget *parent)
     , ui(new Ui::UAmp)
 {
     ui->setupUi(this);
+//    this->setAcceptDrops(true);
+
+    m_playListModel = new QStandardItemModel(this);
+    m_player = new QMediaPlayer(this);          // Init player
+    m_playlist = new QMediaPlaylist(m_player);  // Init playlist
+
+    ui->songs_table->setModel(m_playListModel);
+    ui->songs_table->hideColumn(1);
+    ui->songs_table->verticalHeader()->setVisible(false);
+    ui->songs_table->horizontalHeader()->setVisible(false);
+
+    m_player->setPlaylist(m_playlist);
+    m_player->setVolume(70);
+    m_playlist->setPlaybackMode(QMediaPlaylist::Loop);  // Set circular play mode playlist
+
+    connect(ui->songs_table, &QTableView::doubleClicked, [this](const QModelIndex &index){
+        m_playlist->setCurrentIndex(index.row());
+    });
 }
 
 UAmp::~UAmp()
@@ -13,3 +31,47 @@ UAmp::~UAmp()
     delete ui;
 }
 
+//void UAmp::addNewSong(const QString &path) {
+
+//}
+
+
+void UAmp::on_b_add_clicked() {
+//    if (!m_playlist->isEmpty()) {
+    if (m_playListModel->columnCount() > 0) {
+        m_playlist->clear();
+        m_playListModel->clear();
+    }
+
+    QStringList files = QFileDialog::getOpenFileNames(this,
+                                                      tr("Open files"),
+                                                      QString(),
+                                                      tr("Audio-Files(*.mp3 *.wav *.mp4 *.flac)"));
+
+        foreach (QString filePath, files) {
+            QList<QStandardItem *> items;
+            items.append(new QStandardItem(QDir(filePath).dirName()));
+            m_playListModel->appendRow(items);
+            m_playlist->addMedia(QUrl(filePath));
+        }
+
+}
+
+//void UAmp::dragEnterEvent(QDragEnterEvent *e)
+//{
+//    if (e->mimeData()->hasUrls()) {
+//        e->acceptProposedAction();
+//    }
+//}
+
+//void UAmp::dropEvent(QDropEvent *e)
+//{
+
+//    foreach (const QUrl &url, e->mimeData()->urls()) {
+//        QList<QStandardItem *> items;
+
+//        items.append(new QStandardItem(url.fileName()));
+//        m_playListModel->appendRow(items);
+//        m_playlist->addMedia(url);
+//    }
+//}
--- app/src/uamp.h
+++ app/src/uamp.h
@@ -1,21 +1,41 @@
-#ifndef UAMP_H
-#define UAMP_H
+#pragma once
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QWidget>
+#include <QStandardItemModel>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QFileDialog>
+#include <QDir>
+//#include <QTimer>
+//#include <QMimeData>
+//#include <QDragEnterEvent>
+
+#include "Playlist.h"
+#include "Track.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class UAmp; }
 QT_END_NAMESPACE
 
+
 class UAmp : public QMainWindow
 {
     Q_OBJECT
 
+    Ui::UAmp *ui;
+    QStandardItemModel  *m_playListModel;   // Data Model for Playlist
+    QMediaPlaylist      *m_playlist;
+    QMediaPlayer        *m_player;
+//    void dropEvent(QDropEvent *e);
+//    void dragEnterEvent(QDragEnterEvent *e);
 public:
     UAmp(QWidget *parent = nullptr);
     ~UAmp();
 
-private:
-    Ui::UAmp *ui;
+    void addNewSong(const QString &path);
+
+private slots:
+    void on_b_add_clicked();
 };
-#endif // UAMP_H
